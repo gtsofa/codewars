8d82f0eebc4b67ad7268b21c48b9cfd7acd244c2
Julius Nyule <jlsnyule@yahoo.com>
2018-03-28 09:17:39 +0300

    refactor

--- are_the_numbers_inorder.py
+++ are_the_numbers_inorder.py
@@ -9,3 +9,18 @@ For example:
 in_asc_order([1,2,4,7,19]) # returns True
 in_asc_order([1,2,3,4,5]); // returns True
 '''
+
+def in_asc_order(mylist):
+  a = []
+  for i in range(len(mylist)):
+    for j in range(i+1, len(mylist)):
+      if mylist[i] < mylist[j]:
+        # use the a=[] to add True/False depending on the condition such a = [False,True,True,False] values
+        a.append(True)
+      else:
+        a.append(False)
+  #bool =  [i, j for (mylist[i], mylist[j]) if mylist[i] <]
+  # check to see if there is False/True value in a and return False/True accordingly.
+  if False in a:
+    return False
+  return True

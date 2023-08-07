22516fadbe0ea3575d848aedb1d667f1eb6c4da9
gtsofa <jlsnyule@yahoo.com>
2023-08-07 15:45:52 +0300

    Add access control and  moved the `FizzBuzz` class to its own file

--- swift-challenges/fizzbuzz/fizzbuzzTests/FizzBuzzTests.swift
+++ swift-challenges/fizzbuzz/fizzbuzzTests/FizzBuzzTests.swift
@@ -6,26 +6,7 @@
 //
 
 import XCTest
-
-class FizzBuzz {
-    func evaluate(_ value: Int) -> String {
-        
-        
-        if (value.isMultiple(of: 3) && value.isMultiple(of: 5)) {
-            return "FizzBuzz"
-        }
-        if (value.isMultiple(of: 3)) {
-            return "Fizz"
-            
-        } else if value.isMultiple(of: 5){
-            
-            return "Buzz"
-        }
-        
-        return String(value)
-    }
-}
-
+import fizzbuzz
 
 final class FizzBuzzTests: XCTestCase {
     

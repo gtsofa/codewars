addee9f544a4c8f99635072c18435034b670af80
gtsofa <jlsnyule@yahoo.com>
2023-08-07 14:20:49 +0300

    FizzBuzz returns "Fizz" if is argument multiple of 3

---
+++ swift-challenges/fizzbuzz/fizzbuzzTests/FizzBuzzTests.swift
@@ -0,0 +1,29 @@
+//
+//  FizzBuzzTests.swift
+//  fizzbuzzTests
+//
+//  Created by Julius on 07/08/2023.
+//
+
+import XCTest
+
+class FizzBuzz {
+    func evaluate(_ value: Int) -> String {
+        if value % 3 == 0 {
+            return "Fizz"
+        } else {
+            return ""
+        }
+        
+    }
+}
+
+
+final class FizzBuzzTests: XCTestCase {
+    
+    func test() {
+        let sut = FizzBuzz()
+        XCTAssertEqual(sut.evaluate(3), "Fizz")
+    }
+
+}

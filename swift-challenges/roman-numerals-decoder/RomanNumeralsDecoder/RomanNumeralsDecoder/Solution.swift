4cbe7439970a71d08188aab28ba0d0be856a8f1f
gtsofa <jlsnyule@yahoo.com>
2024-08-16 22:08:31 +0300

    Move `Solution` file to production

--- swift-challenges/roman-numerals-decoder/RomanNumeralsDecoder/RomanNumeralsDecoder/Solution.swift
+++ swift-challenges/roman-numerals-decoder/RomanNumeralsDecoder/RomanNumeralsDecoder/Solution.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+public final class Solution {
+    public init() {}
+    
+    public func solution(_ string: String) -> Int {
+        let romanValues: [Character: Int] = [
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        ]
+        
+        var result = 0
+        let characters = Array(string)
+        
+        for i in 0..<characters.count {
+            let currentValue = romanValues[characters[i]] ?? 0
+            if i < characters.count - 1, currentValue < (romanValues[characters[i + 1]] ?? 0) {
+                result -= currentValue
+            } else {
+                result += currentValue
+            }
+        }
+        return result
+    }
+}

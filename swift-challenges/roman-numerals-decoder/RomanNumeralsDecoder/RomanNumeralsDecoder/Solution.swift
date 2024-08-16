08a4289ca8fe6f4d4e8d67ae8c2a6f1484c0c822
gtsofa <jlsnyule@yahoo.com>
2024-08-16 22:15:43 +0300

    Add another solution

--- swift-challenges/roman-numerals-decoder/RomanNumeralsDecoder/RomanNumeralsDecoder/Solution.swift
+++ swift-challenges/roman-numerals-decoder/RomanNumeralsDecoder/RomanNumeralsDecoder/Solution.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public final class Solution {
+/*public final class Solution {
     public init() {}
     
     public func solution(_ string: String) -> Int {
@@ -33,5 +33,35 @@ public final class Solution {
             }
         }
         return result
+    }
+}*/
+
+//# Another solution
+public final class Solution {
+    public init() {}
+    
+    func value(of numeral: Character) -> Int? {
+        switch numeral {
+        case "I": return 1
+        case "V": return 5
+        case "X": return 10
+        case "L": return 50
+        case "C": return 100
+        case "D": return 500
+        case "M": return 1000
+        default: return nil
+        }
+    }
+    
+    public func solution(_ string: String) -> Int {
+        let numerals = string.compactMap(value)
+        let sum = numerals.reduce(0, +)
+        
+        let difference = zip(numerals, numerals.dropFirst())
+            .filter(<)
+            .map { $0.0 }
+            .reduce(0, +)
+        
+        return sum - difference * 2
     }
 }

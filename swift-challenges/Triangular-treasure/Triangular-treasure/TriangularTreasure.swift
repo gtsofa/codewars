3ac7951c819581237d270ef385f43d93daba1ccc
gtsofa <jlsnyule@yahoo.com>
2024-08-17 22:15:48 +0300

    Add answer statment

--- swift-challenges/Triangular-treasure/Triangular-treasure/TriangularTreasure.swift
+++ swift-challenges/Triangular-treasure/Triangular-treasure/TriangularTreasure.swift
@@ -21,3 +21,9 @@ public final class TriangularTreasure {
 // #Question statement
 // Triangular numbers are so called because of the equilateral triangular shape that they occupy when laid out as dots. i.e.
 // You need to return the nth triangular number. You should return 0 for out of range values:
+
+// # Answer statement
+/*Explanation:
+Triangular Numbers: The nth triangular number can be calculated using the formula n * (n + 1) / 2.
+Out-of-Range Handling: If n is less than or equal to 0, the function returns 0 since there are no triangular numbers for those values.
+This code will correctly return the nth triangular number or 0 for invalid inputs.*/

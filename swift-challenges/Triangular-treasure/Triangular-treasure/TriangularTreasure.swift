34f861f4200d2839a0dda8ff30669ba63a072632
gtsofa <jlsnyule@yahoo.com>
2024-08-17 22:14:43 +0300

    Move `TriangularTreasure` to its own file in production

--- swift-challenges/Triangular-treasure/Triangular-treasure/TriangularTreasure.swift
+++ swift-challenges/Triangular-treasure/Triangular-treasure/TriangularTreasure.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+public final class TriangularTreasure {
+    public init() {}
+    
+    public func triangular(_ n: Int) -> Int  {
+        guard n > 0 else {
+            return 0
+        }
+        return n * (n + 1) / 2
+    }
+}
+
+// #Question statement
+// Triangular numbers are so called because of the equilateral triangular shape that they occupy when laid out as dots. i.e.
+// You need to return the nth triangular number. You should return 0 for out of range values:
--- swift-challenges/Triangular-treasure/Triangular-treasureTests/TriangularTreasureTests.swift
+++ swift-challenges/Triangular-treasure/Triangular-treasureTests/TriangularTreasureTests.swift
@@ -6,16 +6,8 @@
 //
 
 import XCTest
-class TriangularTreasure {
-    func triangular(_ n: Int) -> Int  {
-        guard n > 0 else {
-            return 0
-        }
-        return n * (n + 1) / 2
-    }
-}
-// Triangular numbers are so called because of the equilateral triangular shape that they occupy when laid out as dots. i.e.
-// You need to return the nth triangular number. You should return 0 for out of range values:
+import Triangular_treasure
+
 final class TriangularTreasureTests: XCTestCase {
     func test() {
         let sut = TriangularTreasure()

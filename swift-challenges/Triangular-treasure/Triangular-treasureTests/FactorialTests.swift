2506ac29961567e400827db72fa311f6dd3aa45a
gtsofa <jlsnyule@yahoo.com>
2024-08-21 01:10:53 +0300

    Add more scenarios to guarantee correctness to the factorial of numbers

--- swift-challenges/Triangular-treasure/Triangular-treasureTests/FactorialTests.swift
+++ swift-challenges/Triangular-treasure/Triangular-treasureTests/FactorialTests.swift
@@ -8,11 +8,14 @@
 import XCTest
 
 class Factorial {
-    func factorial(_ n: Int) -> UInt64 {
+    func factorial(_ n: Int) -> Int {
+        //the factorial of `0` is defined as `1`
         if n == 0 {
             return 1
         } else {
-            return 0
+            // The function calls itself with `n-1` until it reaches `0`, where the factorial of `0` is defined as `1`.
+            //The results are then multiplied together as the stack unwinds.
+            return n * factorial(n - 1)
         }
     }
 }
@@ -20,9 +23,15 @@ final class FactorialTests: XCTestCase {
     func test_factorial_deliversResult() {
         let sut = Factorial()
         
-        let result = sut.factorial(0)
         
-        XCTAssertEqual(result, 1)
+        XCTAssertEqual(sut.factorial(0), 1)
+        XCTAssertEqual(sut.factorial(1), 1)
+        XCTAssertEqual(sut.factorial(2), 2)
+        XCTAssertEqual(sut.factorial(3), 6)
+        XCTAssertEqual(sut.factorial(4), 24)
+        XCTAssertEqual(sut.factorial(5), 120)
+        XCTAssertEqual(sut.factorial(6), 720)
+        XCTAssertEqual(sut.factorial(7), 5040)
         
     }
 

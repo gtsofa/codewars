8968652674a060975e28b57e651ff8fe35711469
gtsofa <jlsnyule@yahoo.com>
2024-08-21 00:05:56 +0300

    Add more scenarios for a better test coverage

--- swift-challenges/Triangular-treasure/Triangular-treasureTests/SumofoddnumbersTests.swift
+++ swift-challenges/Triangular-treasure/Triangular-treasureTests/SumofoddnumbersTests.swift
@@ -9,18 +9,25 @@ import XCTest
 
 class Sumofoddnumbers {
     func sumOddNumbers(_ row: Int) -> Int {
-        return 1
+        return row * row * row
     }
 }
 
 final class SumofoddnumbersTests: XCTestCase {
-    func test_sumOddNumbers_deliversOne() {
+    func test_sumOddNumbers_deliversCorrectResults() {
         let sut = Sumofoddnumbers()
-        let result = sut.sumOddNumbers(1)
         
-        XCTAssertEqual(result, 1)
-        
-        
+        XCTAssertEqual(sut.sumOddNumbers(1), 1)
+        XCTAssertEqual(sut.sumOddNumbers(2), 8)
+        XCTAssertEqual(sut.sumOddNumbers(3), 27)
+        XCTAssertEqual(sut.sumOddNumbers(4), 64)
+        XCTAssertEqual(sut.sumOddNumbers(13), 2197)
+        XCTAssertEqual(sut.sumOddNumbers(19), 6859)
+        XCTAssertEqual(sut.sumOddNumbers(41), 68921)
+        XCTAssertEqual(sut.sumOddNumbers(42), 74088)
+        XCTAssertEqual(sut.sumOddNumbers(74), 405224)
+        XCTAssertEqual(sut.sumOddNumbers(86), 636056)
+        XCTAssertEqual(sut.sumOddNumbers(93), 804357)
+        XCTAssertEqual(sut.sumOddNumbers(101), 1030301)
     }
-
 }

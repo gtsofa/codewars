3b94a299824b785bd211d513cb6a321a1e82a21c
gtsofa <jlsnyule@yahoo.com>
2024-08-21 00:49:01 +0300

    First factorial number

--- swift-challenges/Triangular-treasure/Triangular-treasureTests/FactorialTests.swift
+++ swift-challenges/Triangular-treasure/Triangular-treasureTests/FactorialTests.swift
@@ -7,29 +7,23 @@
 
 import XCTest
 
-final class FactorialTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class Factorial {
+    func factorial(_ n: Int) -> UInt64 {
+        if n == 0 {
+            return 1
+        } else {
+            return 0
         }
+    }
+}
+final class FactorialTests: XCTestCase {
+    func test_factorial_deliversResult() {
+        let sut = Factorial()
+        
+        let result = sut.factorial(0)
+        
+        XCTAssertEqual(result, 1)
+        
     }
 
 }

15b8928787e3b7866fb09eaaa5be5e2f8dcf29ec
gtsofa <jlsnyule@yahoo.com>
2024-08-22 01:00:54 +0300

    Refactor `whatCentury()` to add a suffix to the century

--- swift-challenges/Triangular-treasure/Triangular-treasureTests/CenturyTests.swift
+++ swift-challenges/Triangular-treasure/Triangular-treasureTests/CenturyTests.swift
@@ -10,10 +10,25 @@ import XCTest
 public class Century {
     func whatCentury(_ year: String) -> String {
         guard let yearInt = Int(year) else {
-            return "0"
+            return ""
         }
         // Formulae: Adds 99 to the year and then performs integer division by 100 to calculate the century.
-        return String((yearInt + 99 ) / 100)
+        let century = (yearInt + 99) / 100
+        let suffix: String
+        
+        switch century % 10 {
+        case 1 where century % 100 != 11:
+            suffix = "st"
+        case 2 where century % 100 != 12:
+            suffix = "nd"
+            
+        case 3 where century % 100 != 13:
+            suffix = "rd"
+        default:
+            suffix = "th"
+        }
+        
+        return "\(century)\(suffix)"
     }
 }
 
@@ -21,11 +36,11 @@ final class CenturyTests: XCTestCase {
     func test_whatCentury_returnsTheCenturyOfInputYear() {
         let sut = Century()
         
-        XCTAssertEqual(sut.whatCentury("1901"), "20")
-        XCTAssertEqual(sut.whatCentury("1999"), "20")
-        XCTAssertEqual(sut.whatCentury("2011"), "21")
-        XCTAssertEqual(sut.whatCentury("2154"), "22")
-        XCTAssertEqual(sut.whatCentury("2259"), "23")
+        XCTAssertEqual(sut.whatCentury("1901"), "20th")
+        XCTAssertEqual(sut.whatCentury("1999"), "20th")
+        XCTAssertEqual(sut.whatCentury("2011"), "21st")
+        XCTAssertEqual(sut.whatCentury("2154"), "22nd")
+        XCTAssertEqual(sut.whatCentury("2259"), "23rd")
     }
 
 }

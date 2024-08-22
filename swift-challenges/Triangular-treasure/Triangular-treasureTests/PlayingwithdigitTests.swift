2a5f6264cb3fd316d636acfef13932d21b2be2f9
gtsofa <jlsnyule@yahoo.com>
2024-08-23 00:17:41 +0300

    Move `Playingwithdigit` to a separate file on prdouction

--- swift-challenges/Triangular-treasure/Triangular-treasureTests/PlayingwithdigitTests.swift
+++ swift-challenges/Triangular-treasure/Triangular-treasureTests/PlayingwithdigitTests.swift
@@ -6,19 +6,7 @@
 //
 
 import XCTest
-
-public class Playingwithdigit {
-    func digPow(for number: Int, using power: Int) -> Int {
-        ////The number numer is converted into an array of its digits
-        let digits = String(number).compactMap { Int(String($0)) }
-        let sum = digits.enumerated().reduce(0) { acc, pair in
-            let (index, digit) = pair
-            return acc + Int(pow(Double(digit), Double(power + index )))
-        }
-        ////For each digit, raise it to the power of its position (starting from power) and add it to the sum.
-        return sum % number == 0 ? sum / number : -1
-    }
-}
+import Triangular_treasure
 
 final class PlayingwithdigitTests: XCTestCase {
     func test_digPow_returnTheSumOfN() {

2dfb7e2696141049ccbf8efcdcbd12bc3ffddd91
gtsofa <jlsnyule@yahoo.com>
2024-08-23 00:23:33 +0300

    Move `Century` to a separate file on production

--- swift-challenges/Triangular-treasure/Triangular-treasureTests/CenturyTests.swift
+++ swift-challenges/Triangular-treasure/Triangular-treasureTests/CenturyTests.swift
@@ -6,31 +6,7 @@
 //
 
 import XCTest
-
-public class Century {
-    func whatCentury(_ year: String) -> String {
-        guard let yearInt = Int(year) else {
-            return ""
-        }
-        // Formulae: Adds 99 to the year and then performs integer division by 100 to calculate the century.
-        let century = (yearInt + 99) / 100
-        let suffix: String
-        
-        switch century % 10 {
-        case 1 where century % 100 != 11:
-            suffix = "st"
-        case 2 where century % 100 != 12:
-            suffix = "nd"
-            
-        case 3 where century % 100 != 13:
-            suffix = "rd"
-        default:
-            suffix = "th"
-        }
-        
-        return "\(century)\(suffix)"
-    }
-}
+import Triangular_treasure
 
 final class CenturyTests: XCTestCase {
     func test_whatCentury_returnsTheCenturyOfInputYear() {

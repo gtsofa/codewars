fb7d6c91f4007a413d5bf779bcaf6ec88b695d47
gtsofa <jlsnyule@yahoo.com>
2024-08-23 22:56:55 +0300

    Refactor first test to show correct intent

--- swift-challenges/Triangular-treasure/Triangular-treasureTests/BinarySearchTests.swift
+++ swift-challenges/Triangular-treasure/Triangular-treasureTests/BinarySearchTests.swift
@@ -12,7 +12,8 @@ final class BinarySearchTests: XCTestCase {
     func test_binarySearch_returnsTheCorrectGuess() {
         let sut = BinarySearch()
         let my_list = [1, 3, 5, 7, 9]
-        XCTAssertEqual(sut.binary_search(list: my_list, item: 6), nil)
+        
+        XCTAssertEqual(sut.binary_search(list: my_list, item: 5), 2)
     }
     
     func test_binarySearch_returnsNilWhenGuessNotFound() {

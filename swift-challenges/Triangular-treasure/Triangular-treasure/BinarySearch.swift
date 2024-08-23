6271797a8f6e1fcac128725bdee64da4f09470a9
gtsofa <jlsnyule@yahoo.com>
2024-08-23 22:43:59 +0300

    The `binary_search()` returns the correct found guess

--- swift-challenges/Triangular-treasure/Triangular-treasure/BinarySearch.swift
+++ swift-challenges/Triangular-treasure/Triangular-treasure/BinarySearch.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+
+public class BinarySearch {
+    public init() {}
+    
+    public func binary_search(list: [Int], item: Int) -> Int? {
+        var low = 0
+        var high = list.count - 1
+        
+        while low <= high {
+            let mid = low + high
+            let guess = list[mid]
+            if guess == item {
+                return mid
+            }
+            if guess > item {
+                high = mid - 1
+            } else {
+                low = mid + 1
+            }
+        }
+        return nil
+    }
+}

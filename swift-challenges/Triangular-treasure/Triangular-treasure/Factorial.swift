f1369390b9cadaa164592be96afee76a49488162
gtsofa <jlsnyule@yahoo.com>
2024-08-25 22:07:09 +0300

    Move `Factorial` to a separate file in production

--- swift-challenges/Triangular-treasure/Triangular-treasure/Factorial.swift
+++ swift-challenges/Triangular-treasure/Triangular-treasure/Factorial.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+public class Factorial {
+    public init () {}
+    
+    public func factorial(_ n: Int) -> UInt64 {
+        //the factorial of `0` is defined as `1`
+        if n == 0 {
+            return 1
+        } else {
+            // The function calls itself with `n-1` until it reaches `0`, where the factorial of `0` is defined as `1`.
+            //The results are then multiplied together as the stack unwinds.
+            return UInt64(n) * factorial(n - 1)
+        }
+    }
+}
--- swift-challenges/Triangular-treasure/Triangular-treasureTests/FactorialTests.swift
+++ swift-challenges/Triangular-treasure/Triangular-treasureTests/FactorialTests.swift
@@ -6,19 +6,8 @@
 //
 
 import XCTest
+import Triangular_treasure
 
-class Factorial {
-    func factorial(_ n: Int) -> UInt64 {
-        //the factorial of `0` is defined as `1`
-        if n == 0 {
-            return 1
-        } else {
-            // The function calls itself with `n-1` until it reaches `0`, where the factorial of `0` is defined as `1`.
-            //The results are then multiplied together as the stack unwinds.
-            return UInt64(n) * factorial(n - 1)
-        }
-    }
-}
 final class FactorialTests: XCTestCase {
     func test_factorial_deliversResult() {
         let sut = Factorial()

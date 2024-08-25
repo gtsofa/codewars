578762ecdfe850575a2a45b80f3bf71c9828446f
gtsofa <jlsnyule@yahoo.com>
2024-08-25 22:01:00 +0300

    Add more scenarios to cover different edge cases

--- swift-challenges/Triangular-treasure/Triangular-treasureTests/RecursionTests.swift
+++ swift-challenges/Triangular-treasure/Triangular-treasureTests/RecursionTests.swift
@@ -8,8 +8,12 @@
 import XCTest
 
 public class Recursion {
-    func fact(_ fact: Int) -> Int {
-        return 1 
+    func fact(_ number: Int) -> Int {
+        if number == 1 {
+            return 1
+        } else {
+            return number * fact(number - 1)
+        }
     }
 }
 
@@ -17,6 +21,8 @@ final class RecursionTests: XCTestCase {
     func test_fact_returnsCorrectFactoriaValue() {
         let sut = Recursion()
         XCTAssertEqual(sut.fact(1), 1)
+        XCTAssertEqual(sut.fact(2), 2)
+        XCTAssertEqual(sut.fact(3), 6)
     }
 
 }

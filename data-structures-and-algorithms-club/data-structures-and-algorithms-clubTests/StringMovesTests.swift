6c6079c51b835b2ea267261bdd05c065e4e268dc
gtsofa <jlsnyule@yahoo.com>
2024-09-10 21:11:18 +0300

    refactor the `canMove` function to capture more edge cases

--- data-structures-and-algorithms-club/data-structures-and-algorithms-clubTests/StringMovesTests.swift
+++ data-structures-and-algorithms-club/data-structures-and-algorithms-clubTests/StringMovesTests.swift
@@ -9,7 +9,25 @@ import XCTest
 
 class StringMoves {
     func canMove(_ str: String) -> Int {
-        return 2
+        let characterCount = str.count
+        var successfulMoves = 0
+        let characters = Array(str) // 'ciao' --> ['c', 'i', 'a', 'o']
+        
+        for i in 0..<characterCount {
+            if characters[i] == "<" {
+                if i > 0 && characters[i-1] != "<" && characters[i-1] != ">" {
+                    successfulMoves += 1
+                }
+            } else if characters[i] == ">" {
+                if i < characterCount - 1 && characters[i+1] != "<" && characters[i+1] != ">" {
+                    successfulMoves += 1
+                }
+            } else if characters[i] == "^" || characters[i] == "v" {
+                successfulMoves += 1
+            }
+        }
+        return successfulMoves
+        
     }
 }
 
@@ -17,6 +35,8 @@ final class StringMovesTests: XCTestCase {
     func test_canMove_returnsSuccessMoves() {
         let sut = StringMoves()
         
-        XCTAssertEqual(sut.canMove("<>^V"), 2)
+        XCTAssertEqual(sut.canMove("><^v"), 2)
+        XCTAssertEqual(sut.canMove("<<^<v>>"), 5)//6
+        XCTAssertEqual(sut.canMove("><><"), 0)
     }
 }

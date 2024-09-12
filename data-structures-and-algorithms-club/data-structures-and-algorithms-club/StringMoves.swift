fac6aea6d05b1caba22d2e84ede480a9ee077eb4
gtsofa <jlsnyule@yahoo.com>
2024-09-12 16:21:46 +0300

    Move StringMoves class to its own file in production

--- data-structures-and-algorithms-club/data-structures-and-algorithms-club/StringMoves.swift
+++ data-structures-and-algorithms-club/data-structures-and-algorithms-club/StringMoves.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+class StringMoves {
+    func canMove(_ str: String) -> Int {
+        let characterCount = str.count
+        var successfulMoves = 0
+        let characters = Array(str) // 'ciao' --> ['c', 'i', 'a', 'o']
+        
+        for i in 0..<characterCount {
+            if characters[i] == "<" {
+                if i > 0 && characters[i-1] != "<" && characters[i-1] != ">" {
+                    successfulMoves += 1
+                }
+            } else if characters[i] == ">" {
+                if i < characterCount - 1 && characters[i+1] != "<" && characters[i+1] != ">" {
+                    successfulMoves += 1
+                }
+            } else if characters[i] == "^" || characters[i] == "v" {
+                successfulMoves += 1
+            }
+        }
+        return successfulMoves
+        
+    }
+}
--- data-structures-and-algorithms-club/data-structures-and-algorithms-clubTests/StringMovesTests.swift
+++ data-structures-and-algorithms-club/data-structures-and-algorithms-clubTests/StringMovesTests.swift
@@ -6,30 +6,7 @@
 //
 
 import XCTest
-
-class StringMoves {
-    func canMove(_ str: String) -> Int {
-        let characterCount = str.count
-        var successfulMoves = 0
-        let characters = Array(str) // 'ciao' --> ['c', 'i', 'a', 'o']
-        
-        for i in 0..<characterCount {
-            if characters[i] == "<" {
-                if i > 0 && characters[i-1] != "<" && characters[i-1] != ">" {
-                    successfulMoves += 1
-                }
-            } else if characters[i] == ">" {
-                if i < characterCount - 1 && characters[i+1] != "<" && characters[i+1] != ">" {
-                    successfulMoves += 1
-                }
-            } else if characters[i] == "^" || characters[i] == "v" {
-                successfulMoves += 1
-            }
-        }
-        return successfulMoves
-        
-    }
-}
+@testable import data_structures_and_algorithms_club
 
 final class StringMovesTests: XCTestCase {
     func test_canMove_returnsSuccessMoves() {

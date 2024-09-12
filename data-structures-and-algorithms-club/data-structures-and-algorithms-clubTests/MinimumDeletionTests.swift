0ab2745dc01866afcce62311ab944536f76006f8
gtsofa <jlsnyule@yahoo.com>
2024-09-12 15:59:51 +0300

    System delivers correct number of letter deletions in a string

--- data-structures-and-algorithms-club/data-structures-and-algorithms-clubTests/MinimumDeletionTests.swift
+++ data-structures-and-algorithms-club/data-structures-and-algorithms-clubTests/MinimumDeletionTests.swift
@@ -7,29 +7,31 @@
 
 import XCTest
 
-final class MinimumDeletionTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+struct MinimumDeletion {
+    func minDeletion(_ str: String) -> Int {
+        var frequency = [Character: Int]()
+        
+        for char in str {
+            frequency[char, default: 0] += 1
         }
+        
+        var deletions = 0
+        
+        for (_, count) in frequency {
+            if count % 2 != 0 {
+                deletions += 1
+            }
+        }
+        
+        return deletions
+    }
+}
+
+final class MinimumDeletionTests: XCTestCase {
+    func test_minDeletion_returnsCorrectNumberOfLetterDeletionsInAString() {
+        let sut = MinimumDeletion()
+        
+        XCTAssertEqual(sut.minDeletion("aaa"), 1)
     }
 
 }

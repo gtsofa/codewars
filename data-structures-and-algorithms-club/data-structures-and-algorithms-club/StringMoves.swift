d7e641348b238cc46c169e81c4cd34aa98a00a7d
gtsofa <jlsnyule@yahoo.com>
2024-09-17 23:05:28 +0300

    Fix `canMove()` to return the correct results

--- data-structures-and-algorithms-club/data-structures-and-algorithms-club/StringMoves.swift
+++ data-structures-and-algorithms-club/data-structures-and-algorithms-club/StringMoves.swift
@@ -9,23 +9,30 @@ import Foundation
 
 class StringMoves {
     func canMove(_ str: String) -> Int {
-        let characterCount = str.count
         var successfulMoves = 0
-        let characters = Array(str) // 'ciao' --> ['c', 'i', 'a', 'o']
+        var characters = Array(str) // 'ciao' --> ['c', 'i', 'a', 'o']
+        let characterCount = characters.count
         
         for i in 0..<characterCount {
-            if characters[i] == "<" {
-                if i > 0 && characters[i-1] != "<" && characters[i-1] != ">" {
-                    successfulMoves += 1
-                }
-            } else if characters[i] == ">" {
-                if i < characterCount - 1 && characters[i+1] != "<" && characters[i+1] != ">" {
-                    successfulMoves += 1
-                }
-            } else if characters[i] == "^" || characters[i] == "v" {
+            if characters[i] == "^" || characters[i] == "v" {
+                successfulMoves += 1
+                characters[i] = "X"
+            }
+            
+            if characters[i] == "<" && i == 0 {
+                successfulMoves += 1
+                characters[i] = "X"
+            }
+            
+            if characters[i] == ">" && i == characterCount - 1 {
+                successfulMoves += 1
+            }
+            
+            if characters[i] == "<" && characters[i - 1] == "X" {
                 successfulMoves += 1
             }
         }
+        
         return successfulMoves
         
     }
--- data-structures-and-algorithms-club/data-structures-and-algorithms-clubTests/StringMovesTests.swift
+++ data-structures-and-algorithms-club/data-structures-and-algorithms-clubTests/StringMovesTests.swift
@@ -13,7 +13,7 @@ final class StringMovesTests: XCTestCase {
         let sut = StringMoves()
         
         XCTAssertEqual(sut.canMove("><^v"), 2)
-        XCTAssertEqual(sut.canMove("<<^<v>>"), 3)//given:6, getting:5, canMove:3
+        XCTAssertEqual(sut.canMove("<<^<v>>"), 6)//given:6, getting:5, canMove:3
         XCTAssertEqual(sut.canMove("><><"), 0)
     }
 }

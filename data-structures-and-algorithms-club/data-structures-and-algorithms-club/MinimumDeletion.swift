865bc6765754488bd0dfc980d30f587ae497c702
gtsofa <jlsnyule@yahoo.com>
2024-10-01 21:51:47 +0300

    Refactor `minDeletion` to be readable

--- data-structures-and-algorithms-club/data-structures-and-algorithms-club/MinimumDeletion.swift
+++ data-structures-and-algorithms-club/data-structures-and-algorithms-club/MinimumDeletion.swift
@@ -9,15 +9,15 @@ import Foundation
 
 struct MinimumDeletion {
     func minDeletion(_ str: String) -> Int {
-        var frequency = [Character: Int]()
+        var frequency = [Character: Int]()//aaa [a: 3]
         
         for char in str {
-            frequency[char, default: 0] += 1
+            frequency[char] = (frequency[char] ?? 0) + 1
         }
-        
+    
         var deletions = 0
         
-        for (_, count) in frequency {
+        for count in frequency.values {
             if count % 2 != 0 {
                 deletions += 1
             }
--- data-structures-and-algorithms-club/data-structures-and-algorithms-clubTests/MinimumDeletionTests.swift
+++ data-structures-and-algorithms-club/data-structures-and-algorithms-clubTests/MinimumDeletionTests.swift
@@ -8,6 +8,23 @@
 import XCTest
 @testable import data_structures_and_algorithms_club
 
+/*
+ In an even word, each letter occurs an even number of times.
+ Write a function solution that, given a string S consisting of N characters, returns the minimum number of letters that must be deleted to obtain an even word.
+ Examples:
+ 1. Given S = "acbcbba", the function should return 1 (one letter b must be deleted).
+ 2. Given S = "axxaxa", your function should return 2 (one letter a and one letter x must be deleted).
+ 3. Given S = "aaaa", your function should return 0 (there is no need to delete any letters).
+ Write an efficient algorithm for the following assumptions:
+ N is an integer within the range [0..200,000];
+ string S is made only of lowercase letters (a−z).
+ 
+ oddnumber;1, 3, 5, 7, 9
+ evennumber n/2 == 0; n % 2 == 0, 2, 4, 6,8
+ s="ciao"
+ [k:v]= s[c] = 1 => ["c": 1, "i": 1, "a": 1, "o": 1]
+ */
+
 final class MinimumDeletionTests: XCTestCase {
     func test_minDeletion_returnsCorrectNumberOfLetterDeletionsInAString() {
         let sut = MinimumDeletion()
